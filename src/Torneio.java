d2526b3bd1a762d1e3e6a4a3d314ac5b2da05044
a1200178 <a1200178@192.168.1.62>
2015-08-14 20:34:22 -0300

    Criando classes: Partida Jogador

--- src/Torneio.java
+++ src/Torneio.java
@@ -1,8 +1,44 @@
+import java.util.LinkedList;
 
 public class Torneio {
  
-
+	private LinkedList<Jogador> jogadores;
+	private LinkedList<Partida> partidas;
+	private Jogador campeao;
+	private int qtdJogadores;
+	
 	public Torneio(){
-		
+		jogadores=new LinkedList<Jogador>();
+		partidas=new LinkedList<Partida>();
+		campeao=new Jogador();
+		qtdJogadores=0;
 	}
+
+	//Cadastra o jogador no campeanato e retorna se o cadastro foi feito com sucesso
+	public boolean insertJogador(Jogador jog){
+		
+		try{
+			jogadores.add(jog);
+		}catch(Exception e){
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public LinkedList readJogadores(){
+		return jogadores;
+	}
+	
+	
+	public Jogador getCampeao() {
+		return campeao;
+	}
+
+	public void setCampeao(Jogador campeao) {
+		this.campeao = campeao;
+	}
+	
+	
+	
 }
