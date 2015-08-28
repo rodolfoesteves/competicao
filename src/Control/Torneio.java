48f2c04778478e86272be30c47d1b6840b27af38
a120047x <a120047x@192.168.1.68>
2015-08-28 19:59:41 -0300

    commit

---
+++ src/Control/Torneio.java
@@ -0,0 +1,84 @@
+package Control;
+import java.util.LinkedList;
+import java.util.Random;
+
+import Model.Jogador;
+import Model.Partida;
+
+public class Torneio {
+ 
+	private LinkedList<Jogador> jogadores;
+	private LinkedList<Partida> partidas;
+	private Jogador campeao;
+	private int qtdJogadores;
+	
+
+	public Torneio(){
+		jogadores=new LinkedList<Jogador>();
+		partidas=new LinkedList<Partida>();
+		campeao=new Jogador();
+		qtdJogadores=0;
+	}
+	
+	//Cadastra o jogador no campeanato e retorna se o cadastro foi feito com sucesso
+	public boolean insertJogador(Jogador jog){
+		
+		try{
+			jogadores.add(jog);
+		}catch(Exception e){
+			return false;
+		}
+		
+		return true;
+	}
+	
+	//Cadastra a partida no campeonato e retorna se o cadastro foi feito com sucesso
+	public boolean insertPartido(Partida partida){
+		
+		try{
+			partidas.add(partida);
+		}catch(Exception e){
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private void gerarPrimeirosJogos(){
+		
+		Random rand=new Random();
+		LinkedList<Jogador> temp=new LinkedList<Jogador>();
+		
+		int  n = 0;
+		
+		while(!jogadores.isEmpty()){
+			n = rand.nextInt(jogadores.size());
+			temp.add(jogadores.get(n));
+			jogadores.remove(n);
+		}
+		
+		jogadores=temp;
+		
+	}
+	
+	//Retorna todos os jogadores
+	public LinkedList readJogadores(){
+		return jogadores;
+	}
+	
+	public int getQtdJogadores() {
+		return qtdJogadores;
+	}
+	public void setQtdJogadores(int qtdJogadores) {
+		this.qtdJogadores = qtdJogadores;
+	}
+	public Jogador getCampeao() {
+		return campeao;
+	}
+	public void setCampeao(Jogador campeao) {
+		this.campeao = campeao;
+	}
+	
+	
+	
+}
---
+++ src/Model/Jogador.java
@@ -0,0 +1,56 @@
+package Model;
+
+import Control.Torneio;
+
+public class Jogador {
+	private String nome;
+	private String rg;
+	private String endereco;
+	private String telefone;
+	
+	public Jogador(){
+		nome="";
+		rg="";
+		endereco="";
+		telefone="";
+	}
+	
+	public boolean cadastrarJogador(){
+		Torneio torneio=new Torneio();
+		return torneio.insertJogador(this);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+		
+	
+}
